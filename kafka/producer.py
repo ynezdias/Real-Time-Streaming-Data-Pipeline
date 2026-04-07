8883bc4eb6e3991f7848b9efdb7af2c0efe9b8c1
Ynez Dias <ynezdias0501@gmail.com>
2026-04-07 11:53:02 -0400

    Edited producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -10,7 +10,7 @@ producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
-
+# editing
 EVENT_TYPES = ["page_view", "search", "add_to_cart", "purchase", "logout"]
 DEVICES = ["mobile", "desktop", "tablet"]
 
--- kafka/topics.sh
+++ kafka/topics.sh
@@ -3,4 +3,4 @@ docker exec -it kafka-kafka-1 kafka-topics \
   --create --topic user-events \
   --bootstrap-server localhost:9092 \
   --partitions 3 \
-  --replication-factor 1
+  --replication-factor 1l
